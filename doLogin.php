7e7a99872017814f32500fb51d10ddadde9cbed7
vergil3 <36623206+vergil3@users.noreply.github.com>
2018-03-08 18:13:34 +0100

    Neuste Version
    Geändert: Einzelne Werte eines Nutzer ausgegeben.
    Und weiters noch einen Zurück Button eingefügt

--- doLogin.php
+++ doLogin.php
@@ -5,26 +5,11 @@
 
 require_once 'db_connect.php';
 require_once 'functions.php';
-session_start();
 
- $username = @$_REQUEST['username'];
- $password = @$_REQUEST['password'];
+$username = @$_REQUEST['username'];
+$password = @$_REQUEST['password'];
 
+$usernameDB = getUsernameDB($username);
 
-$userInfosDB = getUser($username, $password);
-
-
-if($userInfosDB != FALSE){
-        $_SESSION['id'] = $userInfosDB['id']; 
-
-        header("location:start.php");
-	//setcookie("login",1, time() + 1);
-
-}else{
-   // setcookie("login",0,time());
-   echo "Falsches pw";
-   header("location:login.php");
-}
-
-
+echo "hallo";
 ?>
--- login.php
+++ login.php
@@ -2,24 +2,18 @@
 //usage:    Login screen for registered users
 //date:     14.02.2018
 //author:   LR
-
-require_once 'db_connect.php';
-require_once 'functions.php';
-
 ?>
 
 <html>
+    <a href="register_members.php">Registrieren</a>
+    <h3>Anmelden</h3>
+    <hr>
     <head>
-        <link rel="stylesheet" href="css/style.css" type="text/css" /> 
         <title>Login</title>
     </head>
-    <body class="background">
-        <form action="doLogin.php" method="POST">      
-            <table class="menu">
-                <tr></tr>
-            </table>  
-            <h1 id="anmelden">Anmelden</h1>
-            <table id="login">
+    <body>
+        <form action="doLogin.php" method="POST">
+            <table>
                 <tr>
                     <td>Benutzername: </td>
                     <td><input type="text" name="username" placeholder="Benutzername"></td>
@@ -29,14 +23,9 @@ require_once 'functions.php';
                     <td><input type="password" name="password" placeholder="Passwort"></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td id="loginButton"><input type="submit" name="login" value="Login"></td>
-
+                    <td><input type="submit" name="login" value="Login"></td>
+                    <td><a href="passwordReset.php">Passwort vergessen?</a></td>
                 </tr>
-                <tr><td></td></tr>
-                <tr> <td></td><td><a href="passwordReset.php">Passwort vergessen?</a></td></tr>
-                <tr><td></td></tr>
-                <tr><td></td><td><a href="register_members.php">Registrieren</a></td></tr>
             </table>
         </form>
     </body>
